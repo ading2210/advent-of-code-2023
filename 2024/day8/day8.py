3c3491a089394e9f3dbfca5c0c952ef7b1ca4129
ading2210 <ading2019@gmail.com>
2024-12-09 12:10:57 -0500

    add day 8 part 2 solution

--- 2024/day8/day8.py
+++ 2024/day8/day8.py
@@ -4,7 +4,6 @@ import itertools
 lines = pathlib.Path("data.txt").read_text().split("\n")
 
 antennas = {}
-antinodes = set()
 
 for y, line in enumerate(lines):
   for x, char in enumerate(line):
@@ -15,30 +14,41 @@ for y, line in enumerate(lines):
     else:
       antennas[char] = [(x, y)]
 
-for frequency, coords in antennas.items():
-  combinations = itertools.combinations(coords, 2)
-
-  for (x1, y1), (x2, y2) in combinations:
-    diff_x = x1 - x2
-    diff_y = y1 - y2
-
-    new_x1, new_y1 = x1 + diff_x, y1 + diff_y
-    new_x2, new_y2 = x2 - diff_x, y2 - diff_y
-    
-    antinodes.add((new_x1, new_y1))
-    antinodes.add((new_x2, new_y2))
-
-lines_new = [list(line) for line in lines]
-total = 0
-for x, y in antinodes:
+def add_antinode(x, y):
   if x < 0 or x >= len(lines[0]):
-    continue
+    return False
   if y < 0 or y >= len(lines):
-    continue
-  lines_new[y][x] = "#"
-  total += 1
+    return False
+  antinodes.add((x, y))
+  return True
 
-for line in lines_new:
-  print("".join(line))
+def add_line(x, y, diff_x, diff_y, part2=False):
+  while True:
+    x += diff_x
+    y += diff_y
+    in_bounds = add_antinode(x, y)
+    if not in_bounds or not part2:
+      break
 
-print(total)
+def make_antinodes(part2=False):
+  for frequency, coords in antennas.items():
+    pairs = itertools.combinations(coords, 2)
+
+    for (x1, y1), (x2, y2) in pairs:
+      diff_x = x1 - x2
+      diff_y = y1 - y2
+
+      if part2:
+        add_antinode(x1, y1)
+        add_antinode(x2, y2)
+        
+      add_line(x1, y1, diff_x, diff_y, part2)
+      add_line(x2, y2, -diff_x, -diff_y, part2)
+
+antinodes = set()
+make_antinodes()
+print(len(antinodes))
+
+antinodes = set()
+make_antinodes(True)
+print(len(antinodes))
